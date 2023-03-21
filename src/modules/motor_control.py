4433a4992506ba0b954ae0840890d3084f5cc11d
Philip Groß <philip.gross@geow.uni-heidelberg.de>
2023-03-21 11:11:02 +0100

    prepare for merge

--- src/modules/motor_control.py
+++ src/modules/motor_control.py
@@ -134,21 +134,21 @@ def move_to(motor, pos, velocity):
     
     
 ### Motor setup and assignment ###
-port_list, module_list, motor_list = setup_motors()
-motor_L, motor_R = assign_motors(module_list, motor_list)
+# port_list, module_list, motor_list = setup_motors()
+# motor_L, motor_R = assign_motors(module_list, motor_list)
 
-motor = motor_L
+# motor = motor_L
 
-class Motor(TMCM1260):
-    def __init__(self, port, module, motor):
-        self.port = port
-        self.module = module
-        self.motor = motor
-        self.motorID = TMCLModule.get_global_parameter(self.module, self.module.GP0.SerialAddress, bank=0)
+# class Motor(TMCM1260):
+#     def __init__(self, port, module, motor):
+#         self.port = port
+#         self.module = module
+#         self.motor = motor
+#         self.motorID = TMCLModule.get_global_parameter(self.module, self.module.GP0.SerialAddress, bank=0)
 
-m0 = Motor(port_list[0], module_list[0], motor_list[0])        
-m1 = Motor(port_list[1], module_list[1], motor_list[1])
+# m0 = Motor(port_list[0], module_list[0], motor_list[0])        
+# m1 = Motor(port_list[1], module_list[1], motor_list[1])
 
-print(m0.motorID)
-print(m1.motorID)
+# print(m0.motorID)
+# print(m1.motorID)
 #
