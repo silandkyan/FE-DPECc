23dfb3708262431aeb34f081525b4763c7bb79b3
Philip Groß <philip.gross@geow.uni-heidelberg.de>
2023-03-22 14:36:03 +0100

    improve Motor class

--- src/modules/Motor.py
+++ src/modules/Motor.py
@@ -17,6 +17,8 @@ import time
 
 def disconnect_motors():
     '''Disconnection routine; should be run at the end of the program.'''
+    for inst in Motor.instances:
+        inst.motor.stop()
     ConnectionManager().disconnect
     print('Motors disconnected!')
 
@@ -24,19 +26,29 @@ def disconnect_motors():
 ##### Motor class definition #####
 
 class Motor(TMCM1260):
+    instances = []
     def __init__(self, port):
         self.port = port
-        self.interface, self.module, self.motor = self.setup_motors(self.port)
+        self.interface, self.module, self.motor = self.setup_motor(self.port)
+        self.__class__.instances.append(self)
     
     
         ### MOTOR SETUP ###
+        
     def init_drive_settings(self, motor):
-        '''Set initial motor drive settings.'''
+        '''Set initial motor drive settings. Values are in pps and are now scaled 
+        to microstep resolution.'''
         motor.drive_settings.max_current = 50
         motor.drive_settings.standby_current = 0
         motor.drive_settings.boost_current = 0
-        # Microstep resolution:
-        motor.drive_settings.microstep_resolution = motor.ENUM.MicrostepResolution16Microsteps
+        # Fullsteps/revolution:
+        self.fsteps_per_rev = 200
+        # set mstep resolution:
+        self.mstep_res_factor = motor.ENUM.MicrostepResolution16Microsteps
+        motor.drive_settings.microstep_resolution = self.mstep_res_factor
+        # calculate msteps/revolution
+        self.msteps_per_fstep = 2 ** self.mstep_res_factor
+        self.msteps_per_rev = self.msteps_per_fstep * self.fsteps_per_rev
         # Toggle step interpolation (works only with 16 microsteps):
         motor.set_axis_parameter(motor.AP.Intpol, value=1)
         #print(motor, motor.drive_settings)
@@ -44,17 +56,12 @@ class Motor(TMCM1260):
     def init_ramp_settings(self, motor):
         '''Set initial motor ramp settings. Values are in pps and are now scaled 
         to microstep resolution.'''
-        # get mstep resolution factor:
-        self.mstep_res_factor = motor.drive_settings.microstep_resolution
-        # calculate msteps/revolution
-        self.fsteps_per_rev = 200
-        self.msteps_per_rev = 2 ** self.mstep_res_factor * self.fsteps_per_rev
         # set max values for ramp. trailing factors were tested for 16 msteps.
-        motor.linear_ramp.max_velocity = 2000#msteps_per_rev * 10
+        motor.linear_ramp.max_velocity = self.msteps_per_rev * 10
         motor.linear_ramp.max_acceleration = self.msteps_per_rev * 5
         #print(motor, motor.linear_ramp)
             
-    def setup_motors(self, port):
+    def setup_motor(self, port):
         '''Aggregate function that goes through the entire motor setup.'''
         # Establish connection between Trinamic module and USB port:
         self.interface = ConnectionManager('--port ' + port).connect()
@@ -101,11 +108,11 @@ class Motor(TMCM1260):
 
 ### Motor assignment ###
 
-port_list = ConnectionManager().list_connections()
+# port_list = ConnectionManager().list_connections()
 
-m1 = Motor(port_list[0])        
-m2 = Motor(port_list[1])
+# m1 = Motor(port_list[0])        
+# m2 = Motor(port_list[1])
 
-print(m1.status_message())
-print(m2.status_message())
+# print(m1.status_message())
+# print(m2.status_message())
 #
